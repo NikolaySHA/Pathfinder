057d60866d3427c68f8b0b593b59ab7841c519ba
NikolaySHA <nikolaishatov@gmail.com>
2024-06-01 23:12:45 +0300

    Initial commit.

---
+++ src/main/java/softuni/pathfinder/web/UserController.java
@@ -0,0 +1,31 @@
+package softuni.pathfinder.web;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import softuni.pathfinder.web.dto.UserRegisterDTO;
+
+@Controller
+public class UserController {
+    @GetMapping("users/register")
+    public String viewRegister(Model model) {
+        model.addAttribute("registredData", new UserRegisterDTO());
+        return "register";
+    }
+    @PostMapping("users/register")
+    public String doRegister(@Valid UserRegisterDTO userRegisterDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("registerData", userRegisterDTO);
+            return "register";
+        }
+        return "redirect:/users/login";
+    }
+    @GetMapping("users/login")
+    public String viewLogin(){
+        return "login";
+    }
+}
